225152f1a5e1a125720e0c2daaee7ff9040c02be
Ummelila <ummelila01@gmail.com>
2026-05-21 03:25:40 +0500

    Add Terraform and Ansible files

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "devops-server-terraform"
+  }
+}
